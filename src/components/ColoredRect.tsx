cbd2732b5d2b7b4ad022b49de3f47b4ce50d4ed4
Kira <kira.eva@protonmail.com>
2026-03-11 19:03:19 +0000

    implement furniture

--- src/components/ColoredRect.tsx
+++ src/components/ColoredRect.tsx
@@ -1,10 +1,18 @@
 import Konva from "konva";
-import { Component } from "react";
+
+import { Component, useEffect } from "react";
 import { Rect } from "react-konva";
 import { ColorTranslator } from "colortranslator";
 
+interface Vector2d {
+  x: number;
+  y: number;
+}
+
 export class ColoredRect extends Component {
   override state = {
+    x: 20,
+    y: 40,
     color: "green",
     shadowColor: "green",
   };
@@ -23,13 +31,13 @@ export class ColoredRect extends Component {
   override render() {
     return (
       <Rect
-        x={20}
-        y={20}
+        x={this.state.x}
+        y={this.state.y}
         width={50}
         height={50}
         fill={this.state.color}
         shadowColor={this.state.shadowColor}
-        shadowBlur={16}
+        shadowBlur={6}
         onClick={this.handleClick}
       />
     );
--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,16 +1,77 @@
-import React from "react";
-import { Layer, Stage, Text } from "react-konva";
+import { Layer, Rect, Stage, Text } from "react-konva";
 import { ColoredRect } from "./ColoredRect";
+import { Component, useRef, useState, useEffect } from "react";
 
-export class Game extends React.Component {
-  override render() {
-    return (
-      <Stage width={window.innerWidth} height={window.innerHeight}>
-        <Layer>
-          <Text text="click uwu" />
-          <ColoredRect />
-        </Layer>
-      </Stage>
-    );
-  }
+interface Furniture {
+  name: string;
+  x: number;
+  y: number;
+  width: number;
+  height: number;
+  fill: string;
+  id: string;
+  rotation: 0;
+}
+
+const chair: Furniture = {
+  name: "chair",
+  id: "chair_01",
+  x: 60,
+  y: 100,
+  width: 100,
+  height: 90,
+  rotation: 0,
+  fill: "beige",
+};
+
+const initialFurniture: Furniture[] = [chair];
+
+const getCorner = (pivotX: number, pivotY: number, diffX: number, diffY: number, angle: number) => {
+  const distance = Math.sqrt(diffX * diffX + diffY * diffY);
+  angle += Math.atan2(diffY, diffX);
+  const x = pivotX + distance * Math.cos(angle);
+  const y = pivotY + distance * Math.sin(angle);
+  return { x, y };
+};
+
+export function Game() {
+  const [furniture, setFurniture] = useState(initialFurniture);
+  const [selectedIds, setSelectedIds] = useState([]);
+
+  const isSelecting = useRef(false);
+  const furnitureRefs = useRef(new Map());
+
+  const handleDragEnd = () => {};
+  const handleTransformEnd = () => {};
+
+  return (
+    <Stage width={600} height={400}>
+      <Layer>
+        {/* <Text x={16} y={4} fontSize={18} fill="white" text="click uwu" /> */}
+        {/* <ColoredRect /> */}
+
+        {furniture.map((f) => (
+          <Rect
+            key={f.id}
+            id={f.id}
+            x={f.x}
+            y={f.y}
+            width={f.width}
+            height={f.height}
+            fill={f.fill}
+            name={f.name}
+            rotation={f.rotation}
+            draggable
+            ref={(node) => {
+              if (node) {
+                furnitureRefs.current.set(f.id, node);
+              }
+            }}
+            onDragEnd={handleDragEnd}
+            onTransformEnd={handleTransformEnd}
+          />
+        ))}
+      </Layer>
+    </Stage>
+  );
 }
